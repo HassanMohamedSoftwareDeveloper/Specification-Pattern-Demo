dc912ed6d4aed235d1f0089d8c8998d68bb98365
Hassan AbdelFattah <hmohammed.c@tetco.sa>
2021-07-27 15:14:54 +0200

    Add Project

---
+++ SpecificationPattern/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
